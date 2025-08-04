851a4df2a001c2511c0c84b3a90e514d3a831fdb
tantock <tangabrielkl@gmail.com>
2025-08-04 07:40:27 -0400

    Added 4th dimension to projection matrix

--- projection.py
+++ projection.py
@@ -10,22 +10,27 @@ class Projection:
         LEFT = -RIGHT
         TOP = math.tan(render.camera.v_fov / 2)
         BOTTOM = -TOP
+        THERE = math.tan(render.camera.d_fov / 2)
+        HERE = -THERE
 
         m00 = 2 / (RIGHT - LEFT)
         m11 = 2 / (TOP - BOTTOM)
-        m22 = (FAR + NEAR) / (FAR - NEAR)
-        m32 = -2 * NEAR * FAR / (FAR - NEAR)
+        m22 = 2 / (THERE - HERE)
+        m33 = (FAR + NEAR) / (FAR - NEAR)
+        m43 = -2 * NEAR * FAR / (FAR - NEAR)
         self.projection_matrix = np.array([
-            [m00, 0, 0, 0],
-            [0, m11, 0, 0],
-            [0, 0, m22, 1],
-            [0, 0, m32, 0]
+            [m00, 0, 0, 0, 0],
+            [0, m11, 0, 0, 0],
+            [0, 0, m22, 0, 1],
+            [0, 0, 0, m33, 1]
+            [0, 0, 0, m43, 0]
         ])
 
-        HW, HH = render.H_WIDTH, render.H_HEIGHT
+        HW, HH, HD = render.H_WIDTH, render.H_HEIGHT, render.H_DEPTH
         self.to_screen_matrix = np.array([
-            [HW, 0, 0, 0],
-            [0, -HH, 0, 0],
-            [0, 0, 1, 0],
-            [HW, HH, 0, 1]
+            [HW, 0, 0, 0, 0],
+            [0, -HH, 0, 0, 0],
+            [0, 0, HD, 0, 0],
+            [0, 0, 0, 1, 0],
+            [HW, HH, HD, 0, 1]
         ])
