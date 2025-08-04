Fixed 3D rotation matrix to handle 4D vector

matrix_functions.py
@@ -15,28 +15,31 @@ def translate(pos):
 
 def rotate_x(a):
     return np.array([
-        [1, 0, 0, 0],
-        [0, math.cos(a), math.sin(a), 0],
-        [0, -math.sin(a), math.cos(a), 0],
-        [0, 0, 0, 1]
+        [1, 0, 0, 0, 0],
+        [0, math.cos(a), math.sin(a), 0, 0],
+        [0, -math.sin(a), math.cos(a), 0, 0],
+        [0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1]
     ])
 
 
 def rotate_y(a):
     return np.array([
-        [math.cos(a), 0, -math.sin(a), 0],
-        [0, 1, 0, 0],
-        [math.sin(a), 0, math.cos(a), 0],
-        [0, 0, 0, 1]
+        [math.cos(a), 0, -math.sin(a), 0, 0],
+        [0, 1, 0, 0, 0],
+        [math.sin(a), 0, math.cos(a), 0, 0],
+        [0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1]
     ])
 
 
 def rotate_z(a):
     return np.array([
-        [math.cos(a), math.sin(a), 0, 0],
-        [-math.sin(a), math.cos(a), 0, 0],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]
+        [math.cos(a), math.sin(a), 0, 0, 0],
+        [-math.sin(a), math.cos(a), 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1]
     ])
 
 