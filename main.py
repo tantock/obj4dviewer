962a1ef82f0fd2556c567bf6d55db7439dbc04f3
tantock <tangabrielkl@gmail.com>
2025-08-04 11:16:14 -0400

    Added axes

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from render import SoftwareRender
+from object import Axes4
 import math
 
 if __name__ == '__main__':
     app = SoftwareRender()
     tesseract_id = app.load_object_from_file('resources/tesseract.obj4')
     tank_id = app.load_object_from_file('resources/t_34_obj.obj')
+    axis_id = app.add_object(Axes4(app))
     app.get_object(tesseract_id).rotate_y(-math.pi / 4)
     app.get_object(tesseract_id).scale(10)
     app.get_object(tesseract_id).translate([10,10,10,0])
