c09b8bf7e2e423728580766133af22d56bf21654
tantock <tangabrielkl@gmail.com>
2025-08-04 10:59:19 -0400

    Added objects from function

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from render import SoftwareRender
+import math
 
 if __name__ == '__main__':
     app = SoftwareRender()
+    tesseract_id = app.load_object_from_file('resources/tesseract.obj4')
+    tank_id = app.load_object_from_file('resources/t_34_obj.obj')
+    app.get_object(tesseract_id).rotate_y(-math.pi / 4)
+    app.get_object(tesseract_id).scale(10)
+    app.get_object(tesseract_id).translate([10,10,10,0])
+    app.get_object(tank_id).rotate_y(-math.pi / 4)
+    app.get_object(tank_id).scale(1/5)
     app.run()
--- render.py
+++ render.py
@@ -12,20 +12,28 @@ class SoftwareRender:
         self.FPS = 60
         self.screen = pg.display.set_mode(self.RES)
         self.clock = pg.time.Clock()
-        self.objects = []
-        self.create_objects()
+        self.objects = {}
+        self.autoincrement_obj_id = 0
+        self.create_default_scene()
 
-    def create_objects(self):
+    def create_default_scene(self):
         self.camera = Camera(self, [-5, 6, -55, 0])
         self.projection = Projection(self)
-        self.objects.append(self.get_object_from_file('resources/tesseract.obj4'))
-        self.objects[-1].rotate_y(-math.pi / 4)
-        self.objects[-1].scale(10)
-        self.objects[-1].translate([10,10,10,0])
-
-        self.objects.append(self.get_object_from_file('resources/t_34_obj.obj'))
-        self.objects[-1].rotate_y(-math.pi / 4)
-        self.objects[-1].scale(1/5)
+    
+    def load_object_from_file(self, filename) -> int:
+        return self.add_object(self.get_object_from_file(filename))
+    
+    def add_object(self, object:Object) -> int:
+        id = self.autoincrement_obj_id
+        self.objects[id] = object
+        self.autoincrement_obj_id += 1
+        return id
+    
+    def pop_object(self, id:int) -> Object:
+        return self.objects.pop(id)
+    
+    def get_object(self, id:int) -> Object:
+        return self.objects[id]
 
     def get_object_from_file(self, filename):
         vertex, faces = [], []
@@ -43,7 +51,7 @@ class SoftwareRender:
 
     def draw(self):
         self.screen.fill(pg.Color('darkslategray'))
-        for object in self.objects:
+        for object in self.objects.values():
             object.draw()
 
     def run(self):
