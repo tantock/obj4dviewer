9d07ce27092217b8be270f67212c843037b0ce1c
tantock <tangabrielkl@gmail.com>
2025-08-05 19:01:01 -0400

    Refactored

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ if __name__ == '__main__':
     tesseract_id = app.load_object_from_file('resources/tesseract.obj4')
     tank_id = app.load_object_from_file('resources/t_34_obj.obj')
     axis_id = app.add_object(Axes4(app))
-    app.get_object(tesseract_id).rotate_y(-math.pi / 4)
-    app.get_object(tesseract_id).scale(10)
-    app.get_object(tesseract_id).translate([10,10,10,0])
-    app.get_object(tank_id).rotate_y(-math.pi / 4)
-    app.get_object(tank_id).scale(1/5)
+    
+    tesseract = app.get_object(tesseract_id)
+    tesseract.rotate_y(-math.pi / 4)
+    tesseract.scale(10)
+    tesseract.translate([10,10,10,0])
+    
+    tank = app.get_object(tank_id)
+    tank.rotate_y(-math.pi / 4)
+    tank.scale(1/5)
+    
     app.run()
