9143f0933f0e5d35cb67b1a85ec829a8d16fcc56
tantock <tangabrielkl@gmail.com>
2025-08-05 17:00:28 -0400

    Added shift orthogonality test

--- tests/test_projection.py
+++ tests/test_projection.py
@@ -33,4 +33,19 @@ def test_perspective_depth_transform():
     for i in range(3):
         assert np.abs(v1ndc[i]) > np.abs(v2ndc[i])
 
+def test_orthogonality():
+    projection = init()
+    x, y, z, w = -10,-20,-30,-40
+    H = 1
+    v1 = projection.projection_matrix @ np.array([x,y,z,w,H])
+
+    v1ndc = v1/v1[-1]
     
+    for i in range(3):
+        arr = [x,y,z,w,H]
+        arr[i] += 10
+        v2 = projection.projection_matrix @ np.array(arr)
+        v2ndc = v2/v2[-1]
+        for j in range(3):
+            if i != j:
+                assert np.isclose(v1ndc[j],v2ndc[j])
