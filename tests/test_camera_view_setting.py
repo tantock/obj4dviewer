7166c378bc6a02c3aec7fb7a73c16fea19467fb8
tantock <tangabrielkl@gmail.com>
2025-08-06 08:22:33 -0400

    Added camera view settings test

---
+++ tests/test_camera_view_setting.py
@@ -0,0 +1,10 @@
+from obj4dviewer.camera_view_setting import *
+from math import isclose
+
+def test_init():
+    cvs = CameraViewSetting(10,0.5, 0.2, 0.5, 12)
+    assert cvs.h_fov == 10
+    assert isclose(cvs.v_fov, 5)
+    assert isclose(cvs.d_fov, 2)
+    assert cvs.near_plane == 0.5
+    assert cvs.far_plane == 12
