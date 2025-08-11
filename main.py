edeaee24d717353f3806cdc3e6eed119e57fd901
tantock <tangabrielkl@gmail.com>
2025-08-10 23:08:12 -0400

    Replaced tank with teapot

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ from obj4dviewer.object import Axes4
 import math
 
 if __name__ == '__main__':
-    app = SoftwareRender(RenderSettings(1600, 900))
+    app = SoftwareRender(RenderSettings(1600, 900, sky_colour = 'black'))
     tesseract_id = app.scene.load_object_from_file('resources/tesseract.obj4')
-    tank_id = app.scene.load_object_from_file('resources/t_34_obj.obj')
+    teapot_id = app.scene.load_object_from_file('resources/teapot.obj')
     axis_id = app.scene.add_object(Axes4())
     
     tesseract = app.scene.get_object(tesseract_id)
     tesseract.rotate_y(-math.pi / 4)
     tesseract.scale(10)
-    tesseract.translate([10,10,10,0])
+    tesseract.translate([10,0,10,0])
     
-    tank = app.scene.get_object(tank_id)
-    tank.rotate_y(-math.pi / 4)
-    tank.scale(1/5)
+    teapot = app.scene.get_object(teapot_id)
+    teapot.rotate_y(-math.pi / 4)
+    teapot.translate([-10,0,-10,0])
+    teapot.scale(2)
+
+    axis = app.scene.get_object(axis_id)
+    axis.scale(5)
     
     app.run()
