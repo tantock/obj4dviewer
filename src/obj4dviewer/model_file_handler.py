4b9aaae8c364ee9f738fa77b58c9541f754d3662
tantock <tangabrielkl@gmail.com>
2025-08-12 22:29:53 -0400

    Forced None values in obj creation

--- src/obj4dviewer/model_file_handler.py
+++ src/obj4dviewer/model_file_handler.py
@@ -16,11 +16,19 @@ def tokenize_obj_file(filename, extra_dim:list[int]) -> Object:
                 vertex.append([float(i) for i in line.split()[1:]] + extra_dim)
             elif line.startswith('vn '):
                 vertex_normals.append([float(i) for i in line.split()[1:]] + extra_dim)
-            elif line.startswith('f'):
+            elif line.startswith('f '):
                 faces_ = line.split()[1:]
                 faces.append([int(face_.split('/')[0]) - 1 for face_ in faces_])
-            elif line.startswith('c'):
+            elif line.startswith('c '):
                 cells_ = line.split()[1:]
                 cells.append([int(cell_.split('/')[0]) - 1 for cell_ in cells_])
+        if len(vertex) == 0:
+            vertex = None
+        if len(vertex_normals) == 0:
+            vertex_normals = None
+        if len(faces) == 0:
+            faces = None
+        if len(cells) == 0:
+            cells = None
 
     return Object(vertices=vertex, faces=faces, vertex_normals=vertex_normals, cells=cells)
